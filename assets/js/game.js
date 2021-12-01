49ef7ec9f7e3dd407b263e299dc3698403f04b88
TelChance <chancetelford0018@gmail.com>
2021-11-30 18:59:07 -0600

    made the player's reset after each battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,36 +8,37 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function (enemyName) {
-    window.alert("Welcom to Robot Gladiators!");
 
-    enemyHealth = enemyHealth - playerAttack;
+    while(enemyHealth > 0) {
+        enemyHealth = enemyHealth - playerAttack;
 
-    console.log(
-        playerName + "attacked" + enemyName + ". " + enemyName + "now has " + enemyHealth + "health remaning."
-    );
-
-    if (enemyHealth <= 0){
-        window.alert(enemyName + "has died!");
+        console.log(
+            playerName + "attacked" + enemyName + ". " + enemyName + "now has " + enemyHealth + "health remaning."
+        );
+    
+        if (enemyHealth <= 0){
+            window.alert(enemyName + "has died!");
+        }
+    
+        else {
+            window.alert(enemyName + "still has" + enemyHealth + "health left.");
+        }
+    
+        playerHealth = playerHealth - enemyAttack;
+    
+        console.log(
+            enemyName + "attacked" + playerName + ". " + playerName + "now has" + playerHealth + "health remaining."
+        );
+    
+        if (playerHealth <= 0) {
+            window.alert(playerName + "has died!");
+        }
+    
+        else {
+            window.alert(playerName + "still has" + playerHealth + "health left");
+        }
+    };
     }
-
-    else {
-        window.alert(enemyName + "still has" + enemyHealth + "health left.");
-    }
-
-    playerHealth = playerHealth - enemyAttack;
-
-    console.log(
-        enemyName + "attacked" + playerName + ". " + playerName + "now has" + playerHealth + "health remaining."
-    );
-
-    if (playerHealth <= 0) {
-        window.alert(playerName + "has died!");
-    }
-
-    else {
-        window.alert(playerName + "still has" + playerHealth + "health left");
-    }
-};
 
 fight();
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -86,5 +87,7 @@ for(var i = 0; i < enemyNames.length; i++) {
 }
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
